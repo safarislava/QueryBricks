35c09605a4cd52b368e5fb2f09c01bcbfbbd4947
safarislava <safarislava@gmail.com>
2026-06-30 22:31:28 +0300

    impl: Add update query

---
+++ src/main/java/com/querybricks/query/UpdateQuery.java
@@ -0,0 +1,32 @@
+package com.querybricks.query;
+
+import com.querybricks.QueryPart;
+import com.querybricks.condition.Condition;
+import com.querybricks.table.Table;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UpdateQuery implements Query {
+    private final Table table;
+    private final List<ColumnAssignment> assignments;
+    private final Condition condition;
+
+    public UpdateQuery(Table table, List<ColumnAssignment> assignments, Condition condition) {
+        this.table = table;
+        this.assignments = List.copyOf(assignments);
+        this.condition = condition;
+    }
+
+    @Override
+    public String sql() {
+        if (this.assignments.isEmpty()) {
+            throw new IllegalStateException("Update query must specify at least one assignment");
+        }
+        return String.format(
+            "UPDATE %s SET %s WHERE %s",
+            this.table.sql(),
+            this.assignments.stream().map(QueryPart::sql).collect(Collectors.joining(", ")),
+            this.condition.sql()
+        );
+    }
+}
