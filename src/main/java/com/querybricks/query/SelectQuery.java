15fee5f4761dc53999265b8b2c0a9724a921b8ad
safarislava <safarislava@gmail.com>
2026-07-01 23:50:14 +0300

    refactor: Migration to prepared statement, add support to query

--- src/main/java/com/querybricks/query/SelectQuery.java
+++ src/main/java/com/querybricks/query/SelectQuery.java
@@ -1,5 +1,6 @@
 package com.querybricks.query;
 
+import com.querybricks.Bindings;
 import com.querybricks.column.Column;
 import com.querybricks.column.Columns;
 import com.querybricks.table.Table;
@@ -17,6 +18,11 @@ public class SelectQuery implements ResultedQuery {
     @Override
     public String sql() {
         return String.format("SELECT %s FROM %s", this.columns.sql(), this.table.sql());
+    }
+
+    @Override
+    public Bindings bind(Bindings bindings) {
+        return this.table.bind(this.columns.bind(bindings));
     }
 
     @Override
