ae8da498eea68db9233e9baf141a7fcf7a79da95
safarislava <safarislava@gmail.com>
2026-07-02 12:26:20 +0300

    docs: Add javadoc for other

--- src/main/java/com/querybricks/Bindings.java
+++ src/main/java/com/querybricks/Bindings.java
@@ -1,5 +1,14 @@
 package com.querybricks;
 
+/**
+ * Accumulates bound query parameters.
+ */
 public interface Bindings {
+    /**
+     * Binds a new value to the parameters list.
+     *
+     * @param value The value to bind.
+     * @return New Bindings instance with the value added.
+     */
     Bindings with(Object value);
 }
--- src/main/java/com/querybricks/QueryPart.java
+++ src/main/java/com/querybricks/QueryPart.java
@@ -1,6 +1,21 @@
 package com.querybricks;
 
+/**
+ * Represents a part of an SQL query.
+ */
 public interface QueryPart {
+    /**
+     * Generates SQL query string representation.
+     *
+     * @return SQL query string snippet.
+     */
     String sql();
+
+    /**
+     * Binds query parameters of this part to the provided Bindings collector.
+     *
+     * @param bindings The bindings' collector.
+     * @return The updated Bindings instance containing this query part's parameters.
+     */
     Bindings bind(Bindings bindings);
 }
